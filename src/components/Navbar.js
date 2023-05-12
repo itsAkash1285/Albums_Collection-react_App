4faf00c8a0d42924e5d26d26ffaf8b76754fbe14
Akash Patil <patilakash1285@gmail.com>
2023-05-13 01:03:43 +0530

    first commit

---
+++ src/components/Navbar.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom'
+
+//get button name and path for evaery component
+const Navbar = (props) => {
+
+  return (
+    <div className='navbar'>
+      <h2>ALBUMS</h2>
+      <Link to={props.path}><button>{props.page}</button></Link>
+    </div>
+  )
+}
+
+export default Navbar
